3814b32e23a81a70aa45de6c67eb1a7c6ac43397
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-10-29 21:57:29 -0300

    Update settings.py

--- agenda_contactos/settings.py
+++ agenda_contactos/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a!y07t_j-b0q&f5*8)ehs(0gq+-s3mr7=bcvmv%&v199snp#2h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".onrender.com",
